fccb50de95d1f47a6ac2529981daf2762da6e276
yunesh11 <zyuvneshkumar11@gmail.com>
2024-09-01 22:52:19 +0530

    first commit

---
+++ d1.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+    println("Hello, world!!!")
+//     one line comment
+/*	morw than one line comment first line
+    second line*/
+//     below commented lines are wrong 
+    var a=10
+    a=89
+//     a=40.5 
+    print(a)
+//     var try=30
+    var Try=40
+    var `try`=70
+    val pos=77.90
+//     pos=89.90
+    println(a+Try+`try`)
+    println("$a $Try $`try`")
+    
+}
